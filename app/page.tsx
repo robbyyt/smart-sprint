82642fe5179125dd628ab0c2b4c65386a1b68009
Robert Tabacaru <49028058+robbyyt@users.noreply.github.com>
2023-08-29 23:13:13 +0300

    added task list from shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import SprintGridView from '@/components/sprint-capacity/sprint-grid-view';
 import TeamListing from '@/components/teams/team-list';
 import { teamData } from '@/components/teams/mock-data';
+import DataTable from '@/components/task-list/data-table';
+import { columns } from '@/components/task-list/data-table/columns';
+import { sampleTasks } from '@/components/task-list/mock-data';
 
 export default function Home() {
   return (
     <main className='p-12'>
       <SprintGridView />
       <TeamListing teams={teamData} />
+      <DataTable columns={columns} data={sampleTasks} />
     </main>
   );
 }
