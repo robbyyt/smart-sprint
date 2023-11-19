cebf9eab87821d2016c6a463254471392fd4abf8
Robert Tabacaru <49028058+robbyyt@users.noreply.github.com>
2023-11-20 00:28:47 +0200

    fix page typings

--- app/teams/[teamId]/setup/page.tsx
+++ app/teams/[teamId]/setup/page.tsx
@@ -4,7 +4,7 @@ import { TeamId } from '@/lib/db/entities/team';
 import { getTeamById } from '@/lib/db/services/team.service';
 import { setTeamId } from '../context';
 
-export default async function Page({ params }: { params: { teamId: string }; children: React.ReactNode }) {
+export default async function Page({ params }: { params: { teamId: string } }) {
   const numericTeamId: TeamId = Number(params.teamId);
 
   if (!numericTeamId) redirect('/not-found');
